309832236a5857fb157d2acb331a80cb327f0a26
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-07-22 15:44:42 +0800

    add tab icon (dostlogo)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="icon" href="{{ asset('dostlogo.png') }}" type="image/png">
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
